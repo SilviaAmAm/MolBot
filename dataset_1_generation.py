958bb18364b087b8886355a2ba44bec23fd1de3d
Silvia Amabilino <walfits@gmail.com>
2018-07-02 11:47:08 +0100

    first commit

---
+++ dataset_1_generation.py
@@ -0,0 +1,70 @@
+import numpy as np
+import joblib
+
+# Importing the data
+in_d = open("bioactivity_PPARg_filtered.csv", 'r')
+
+molecules = []
+
+for line in in_d:
+    line_split = line.split(",")
+    molecule_raw = line_split[-3]
+    molecule = molecule_raw.strip('"')
+    if molecule == "CANONICAL_SMILES":
+        pass
+    else:
+        molecules.append(molecule)
+
+# Preparing for one hot encoding
+
+all_possible_char = ['G', 'E', 'A']
+max_size = 2
+
+for molecule in molecules:
+    all_char = list(molecule)
+
+    if len(all_char)+2 > max_size:
+        max_size=len(all_char)+2
+
+    unique_char = list(set(all_char))
+    for item in unique_char:
+        if not item in all_possible_char:
+            all_possible_char.append(item)
+
+# Padding
+padded_molecules = []
+for molecule in molecules:
+    molecule = 'G' + molecule
+    molecule = molecule + 'E'
+    if len(molecule) < max_size:
+        padding = int(max_size - len(molecule))
+        for i in range(padding):
+            molecule += 'A'
+    padded_molecules.append(molecule)
+
+idx_to_char = {idx:char for idx, char in enumerate(all_possible_char)}
+char_to_idx = {char:idx for idx, char in enumerate(all_possible_char)}
+
+n_feat = len(all_possible_char)
+n_samples = int(len(molecules))
+
+X = np.zeros((n_samples, max_size, n_feat))
+y = np.zeros((n_samples, max_size, n_feat))
+
+for n in range(n_samples):
+    sample = padded_molecules[n]
+    sample_idx = [char_to_idx[char] for char in sample]
+    input_sequence = np.zeros((max_size, n_feat))
+    for j in range(max_size):
+        input_sequence[j][sample_idx[j]] = 1.0
+    X[n]=input_sequence
+
+    output_sequence = np.zeros((max_size, n_feat))
+    for j in range(max_size-1):
+        output_sequence[j][sample_idx[j+1]] = 1.0
+    y[n] = output_sequence
+
+# np.savez("dataset_1.npz", X, y, idx_to_char, char_to_idx)
+data = {"X":X, "y":y, "idx_to_char":idx_to_char, "char_to_idx":char_to_idx}
+joblib.dump(data, "dataset_1.bz")
+
---
+++ lstm_1.py
@@ -0,0 +1,44 @@
+import numpy as np
+import joblib
+from keras import Sequential
+from keras.layers import LSTM
+from keras.layers import TimeDistributed
+from keras.layers import Dense
+from keras.layers import Activation
+
+
+data = joblib.load("dataset_1.bz")
+X = data["X"]
+y = data["y"]
+idx_to_char = data["idx_to_char"]
+char_to_idx = data["char_to_idx"]
+
+n_samples = X.shape[0]
+max_size = X.shape[1]
+n_feat = X.shape[2]
+
+hidden_neurons_1 = 256
+hidden_neurons_2 = 256
+
+model = Sequential()
+model.add(LSTM(units=hidden_neurons_1, input_shape=(None, n_feat), return_sequences=True))
+model.add(LSTM(units=hidden_neurons_2, input_shape=(None, hidden_neurons_1), return_sequences=True))
+model.add(TimeDistributed(Dense(n_feat)))
+model.add(Activation('softmax'))
+model.compile(loss="categorical_crossentropy", optimizer="rmsprop")
+
+model.fit(X, y, batch_size=10, verbose=1, nb_epoch=3)
+
+X_pred = np.zeros((1, max_size, n_feat))
+y_char = ['G']
+X_pred[0, 0, char_to_idx['G']] = 1
+for i in range(max_size-1):
+    out = model.predict(X_pred[:, :i+1, :])[0][-1]
+    idx_out = np.argmax(out)
+    X_pred[0, i+1, idx_out] = 1
+    if idx_to_char[idx_out] == 'E':
+        break
+    else:
+        y_char.append(idx_to_char[idx_out])
+
+print(y_char)
