b3fee4462bf3d6dc2d4b7206d7c9c1721e07dd1f
Silvia Amabilino <walfits@gmail.com>
2018-07-13 07:25:20 +0100

    Attempt at making things work with the two different idx_to_char dictionaries

--- Model_1.py
+++ Model_1.py
@@ -12,16 +12,16 @@ tensorboard = TensorBoard(log_dir='./tb/model_1',
     write_graph=True, write_images=False)
 callbacks_list = [tensorboard]
 
-data = joblib.load("dataset_1.bz")
-X = data["X"]
-y = data["y"]
+data_1 = joblib.load("dataset_1.bz")
+X_1 = data_1["X"]
+y_1 = data_1["y"]
 
-idx_to_char = data["idx_to_char"]
-char_to_idx = data["char_to_idx"]
+idx_to_char_1 = data_1["idx_to_char"]
+char_to_idx_1 = data_1["char_to_idx"]
 
-n_samples = X.shape[0]
-max_size = X.shape[1]
-n_feat = X.shape[2]
+n_samples = X_1.shape[0]
+max_size = X_1.shape[1]
+n_feat = X_1.shape[2]
 
 hidden_neurons_1 = 256
 hidden_neurons_2 = 256
@@ -38,7 +38,7 @@ model.add(Lambda(lambda x: x / 1))
 model.add(Activation('softmax'))
 model.compile(loss="categorical_crossentropy", optimizer="rmsprop")
 
-model.fit(X, y, batch_size=500, verbose=1, nb_epoch=4, callbacks=callbacks_list)
+model.fit(X_1, y_1, batch_size=500, verbose=1, nb_epoch=4, callbacks=callbacks_list)
 model.save("./saved_models/model_1/model_1.h5")
 
 new_model = load_model("./saved_models/model_1/model_1.h5")
@@ -46,6 +46,8 @@ f = open("./pred_smiles/pred_smiles_1.txt", 'w')
 
 data_2 = joblib.load("dataset_2.bz")
 X_2 = data_2["X"]
+idx_to_char_2 = data_2["idx_to_char"]
+char_to_idx_2 = data_2["char_to_idx"]
 
 
 for i in range(4):
@@ -57,11 +59,11 @@ for i in range(4):
 
     for i in range(X_pred.shape[0]):
         idx = np.argmax(X_pred[i])
-        y_pred += idx_to_char[idx]
+        y_pred += idx_to_char_2[idx]
 
     for i in range(X_original.shape[0]):
         idx = np.argmax(X_original[i])
-        y_orig += idx_to_char[idx]
+        y_orig += idx_to_char_2[idx]
 
     X_pred = np.reshape(X_pred, (1, X_pred.shape[0], X_pred.shape[1]))
     X_pred_temp = X_pred
@@ -69,7 +71,7 @@ for i in range(4):
     while( y_pred[-1] != 'E'):
         out = new_model.predict(X_pred_temp)
         out_idx = np.argmax(out[0])
-        y_pred += idx_to_char[out_idx]
+        y_pred += idx_to_char_1[out_idx]
         X_pred_temp[:, :-1, :] = X_pred[:, 1:, :]
         y_pred_hot = np.zeros((1, X_pred.shape[-1]))
         y_pred_hot[:, out_idx] = 1
